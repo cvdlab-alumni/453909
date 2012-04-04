8022687583af380976b6508b50c374df4bf8a7bc
Francesca Giacobbe <francygiacob@hotmail.it>
2012-04-04 18:25:26 +0200

    Homework1

---
+++ 2012-04-03/exercise2.js
@@ -0,0 +1,43 @@
+
+
+
+
+// piano
+
+
+
+var f1 = SIMPLEX_GRID ([[-1,39],[1],[1]]);
+
+
+var f2 = SIMPLEX_GRID ([[1],[2],[1]]);
+
+
+var f3 = SIMPLEX_GRID ([[-14,26],[-1,4],[1]]);
+
+
+var f4 = SIMPLEX_GRID ([[-14,39],[-4,2],[1]]);
+
+
+var f5 = SIMPLEX_GRID ([[-14,38],[-4,15],[1]]);
+
+
+
+var f6 = SIMPLEX_GRID ([[-14,39],[-4,2],[1]]);
+
+
+var f7 = SIMPLEX_GRID ([[-1,46],[-10,13],[1]]);
+
+
+var f8 = SIMPLEX_GRID ([[-1,8],[-17,9],[1]]);
+
+
+var f9 = SIMPLEX_GRID ([[-1,20],[-1,14],[0.1]]);
+
+
+
+var floor = STRUCT ([f1,f2,f3,f4,f5,f6,f7,f8,f9]);
+
+DRAW (floor)
+
+
+
