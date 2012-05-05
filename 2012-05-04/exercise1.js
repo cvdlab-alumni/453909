f6b6d3943925e45b177a9b22bf1c16218a8888ed
Francesca Giacobbe <francygiacob@hotmail.it>
2012-05-05 11:46:15 +0200

    exercise1

--- 2012-05-04/exercise1.js
+++ 2012-05-04/exercise1.js
@@ -27,8 +27,9 @@ var controls1= [c0,c1,c2,c3,c4];
 
 var domain2 = DOMAIN([[0, 1], [0, 1]])([15, 70]); 
 var surf1 = BEZIER(S1)(controls1);
-var wingLDX = MAP(surf1)(domain2);
-
+var wingDx = MAP(surf1)(domain2);
+// DRAW(wingDx)
+//ala in basso a destra
 
 var p5 = p0.map(function(p){return[p[0], p[1]-0.5, p[2]]});
 var p6 = p0.map(function(p){return[p[0], p[1]-1.5, p[2]]});
@@ -47,12 +48,16 @@ var controls2= [c6,c7,c8,c9,c10];
 
 
 var surf2 = BEZIER(S1)(controls2);
-var wingLSX = MAP(surf2)(domain2);
+var wingSx = MAP(surf2)(domain2);
+
+// DRAW(wingSx)
+// ala in basso a sinistra
 
 
 
+// ala superiore unica
 
-var wingUDX=T([2])([1])(wingLDX);
+var wingUdx=T([2])([1])(wingDx);
 var p5 = p0.map(function(p){return[p[0], p[1]-0.5, p[2]]});
 var p6 = p0.map(function(p){return[p[0], p[1]-1.5, p[2]]});
 var p7 = p0.map(function(p){return[p[0], p[1]-2.5, p[2]]});
@@ -69,8 +74,8 @@ var c9 = BEZIER(S0)(p9);
 
 
 var surf2 = BEZIER(S1)([c0, c5,c6, c7, c8, c9, c10]);
-var wingUSX =T([2])([1])(MAP(surf2)(domain2));
-var wings = STRUCT([wingUSX, wingLDX, wingUDX, wingLSX]);
+var wingUsx =T([2])([1])(MAP(surf2)(domain2));
+var wings = STRUCT([wingUsx, wingDx, wingUdx, wingSx]);
 
 
 
