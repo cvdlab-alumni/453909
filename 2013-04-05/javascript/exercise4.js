e664041f2c0784056876e0e41441b0e450455676
Francesca Giacobbe <francygiacob@hotmail.it>
2013-04-06 18:43:42 +0200

    exercise4

--- 2013-04-05/javascript/exercise4.js
+++ 2013-04-05/javascript/exercise4.js
@@ -1,1 +1,1 @@
-/Define and color (BLACK) the models of (some) windows, and instance them within the building model./
+//Define and color (BLACK) the models of (some) windows, and instance them within the building model.
